cfeff11132546fb950aad025d4d92ed97accce7d
unknown <moyuan@gmail.com>
2023-10-16 11:35:59 +0800

    图片切换显示

--- src/views/ImgViewAndEdit.vue
+++ src/views/ImgViewAndEdit.vue
@@ -1,47 +1,58 @@
 <template>
     <div style="position: relative; height: 100%; width: 100%;">
         <div class="flex-contain-img">
-            <div class="flex-img shrink glow " style="max-width: 80%;">
-                <div ref="img_contain" style="display: flex; margin: 0 auto; position: relative; ">
-                    <div id="img-parent" style="display: flex ;  position: relative; max-height: 100%; max-width: 100%;">
+            <div ref="img_contain"  class="flex-img shrink glow " style="max-width: 80%;">
+                <div  style="display: flex; margin: 0 auto; position: relative; ">
+                    <div  style="display: flex ;  position: relative; max-height: 100%; max-width: 100%;">
+
                         <!-- <template v-if="CurrentGenImageList[selectedCurrentImgIndex].hr_enable_size == 1">
                             <img :src="CurrentSelectedImgURL" style="position: relative; left: 0px; width: 700px; height: auto; object-fit: contain;">
                         </template> -->
                         <!-- <template v-else> -->
-                        <img :src="CurrentSelectedImgURL">
+                        <div style="display: flex; flex-direction: column;">
+                            <div style="display: flex; flex-grow: 1;"></div>
+
+                            <div id="img-parent" style="position: relative; margin: auto;">
+                                <img :src="CurrentSelectedImgURL">
+                                <div class="tools" @click="gallery.view(0)"
+                                    style="position: absolute; bottom: 0px; right: 0px; height: 100%; width: 100%;">
+                                    <div style="position: absolute; bottom: 10px; right: 10px;">
+                                        <div class="cursor-pointer tools-item"
+                                            style="display: inline-block; background-color: #333d; margin: 0 10px; padding: 8px; line-height: 0; border-radius: 30px;">
+                                            <a :href="CurrentSelectedImgURL" download="AI creative engine">
+
+
+                                                <el-tooltip effect="dark" content="下载图片" placement="top"
+                                                    style="font-size: 20px;">
+                                                    <el-icon color="#fffd" :size="25">
+                                                        <Download />
+                                                    </el-icon>
+                                                </el-tooltip>
+
+                                            </a>
+                                        </div>
+
+                                        <div class="cursor-pointer tools-item"
+                                            style="display: inline-block; background-color: #333d; margin: 0; padding: 8px; line-height: 0; border-radius: 30px;">
+                                            <div @click="hr()">
+                                                <el-tooltip effect="dark" content="高清放大此图" placement="top"
+                                                    style="font-size: 20px;">
+                                                    <el-icon color="#fffd" :size="25">
+                                                        <Search />
+                                                    </el-icon>
+                                                </el-tooltip>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="display: flex; flex-grow: 1;"></div>
+                        </div>
                         <!-- </template> -->
                         <!-- <img src="../assets/png/769x960.png" > -->
                         <!-- <img src="../assets/png/1022x571.jpg" > -->
 
-                        <div class="tools" @click="gallery.view(0)"
-                            style="position: absolute; bottom: 0px; right: 0px; height: 100%; width: 100%;">
-                            <div style="position: absolute; bottom: 10px; right: 10px;">
-                                <div class="cursor-pointer tools-item"
-                                    style="display: inline-block; background-color: #333d; margin: 0 10px; padding: 8px; line-height: 0; border-radius: 30px;">
-                                    <a :href="CurrentSelectedImgURL" download="AI creative engine">
 
-
-                                        <el-tooltip effect="dark" content="下载图片" placement="top" style="font-size: 20px;">
-                                            <el-icon color="#fffd" :size="25">
-                                                <Download />
-                                            </el-icon>
-                                        </el-tooltip>
-
-                                    </a>
-                                </div>
-
-                                <div class="cursor-pointer tools-item"
-                                    style="display: inline-block; background-color: #333d; margin: 0; padding: 8px; line-height: 0; border-radius: 30px;">
-                                    <div @click="hr()">
-                                        <el-tooltip effect="dark" content="高清放大此图" placement="top" style="font-size: 20px;">
-                                            <el-icon color="#fffd" :size="25">
-                                                <Search />
-                                            </el-icon>
-                                        </el-tooltip>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -71,33 +82,45 @@ import { reflushImages } from '@/assets/ImgViewRoll.js'
 import { paramButtonIsShow } from '@/assets/GlobalStatus.js'
 paramButtonIsShow.value = true
 let gallery = null
+let img_contain = ref(null)
 onMounted(() => {
-    gallery = new Viewer(document.getElementById('img-parent'));
+    gallery = new Viewer(document.getElementById('img-parent'), {
+        toolbar: {
+            prev: 0,
+            next: 0
+        },
+        title: false,
+        navbar: false
+    });
     $('.tools-item').click(() => {
         event.stopPropagation()
     })
-})
-let img_contain = ref(null)
+    // 
+    
+})  
 function hr() {
     onSubmit(true, null, CurrentGenImageList.value[selectedCurrentImgIndex.value].seed,
         CurrentGenImageList.value[selectedCurrentImgIndex.value].hr_enable_size)
 }
 
-// watch(CurrentGenImageList, (newVal, oldVal) => {
-//     if (CurrentGenImageList.value[selectedCurrentImgIndex.value].hr_enable_size > 1) {
-//         img_contain.value.classList.add('img_contain_sm')
-//     } else {
-//         img_contain.value.classList.remove('img_contain_sm')
-//     }
-// })
+watch(CurrentGenImageList, (newVal, oldVal) => {
+    if (CurrentGenImageList.value[selectedCurrentImgIndex.value].hr_enable_size > 1) {
+        img_contain.value.style["max-width"] = '70%';
+    } else {
+        img_contain.value.style["max-width"] = '40%';
+    }
+    // gallery.update() // 可以更新组件
 
-// watch(selectedCurrentImgIndex, (newVal, oldVal) => {
-//     if (CurrentGenImageList.value[selectedCurrentImgIndex.value].hr_enable_size > 1) {
-//         img_contain.value.classList.add('img_contain_sm')
-//     } else {
-//         img_contain.value.classList.remove('img_contain_sm')
-//     }
-// })
+}, {deep: true, flush: 'post'})
+
+watch(selectedCurrentImgIndex, (newVal, oldVal) => {
+    if (CurrentGenImageList.value[selectedCurrentImgIndex.value].hr_enable_size > 1) {
+        img_contain.value.style["max-width"] = '70%';
+    } else {
+        img_contain.value.style["max-width"] = '40%';
+    }
+    gallery.update()
+}, { flush: 'post'})
 
 </script>
 
@@ -161,6 +184,9 @@ function hr() {
 
 
 .tools {
+    display: flex;
+    max-height: 100%;
+    max-width: 100%;
     opacity: 0;
 }
 
